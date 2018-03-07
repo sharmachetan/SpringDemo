98eeb8ccd247f74d2f2b8fa95d66294ca318bd8d
sharmachetan <sharmachetan6@gmail.com>
2018-03-07 10:24:50 +0530

    Add files via upload

---
+++ src/com/demo/Welcome.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Welcome {
+	String message;
+
+	public void getMessage() {
+		
+		
+		 System.out.println(" Welcome to Spring Demo:" + message);;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
